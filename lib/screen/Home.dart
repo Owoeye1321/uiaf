6ff0492c14cf2a43bee0db1caefff9c664f416f9
Owoeye Samuel <owoeye1321@gmail.com>
2024-09-21 14:12:00 +0100

    gooodddaaam progress 〽️

--- lib/screen/Home.dart
+++ lib/screen/Home.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:uiaf/data/dummy_items.dart';
 import 'package:uiaf/widgets/grocery_item.dart';
+import 'package:uiaf/widgets/new_item.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({
     super.key,
   });
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  void _addNewItem() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) {
+      return const NewItem();
+    }));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              _addNewItem;
+            },
+            icon: Icon(Icons.add),
+          ),
+        ],
         centerTitle: false,
         title: const Text("Your Groceries"),
       ),
